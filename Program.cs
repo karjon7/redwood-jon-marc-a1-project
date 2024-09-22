325ab6b7f165630678af2f382aec853341fed295
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-09-22 17:40:21 -0400

    Initialize variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿
+bool is_playing = true;
+
+string player_name;
+string current_location;
+
+//Inventory
+bool has_forest_hut_key = false;
+bool has_fishing_hut_key = false;
+bool has_graveyard_hut_key = false;
+
