71d53556f10ea08aac3f0cd3fd30ce81690789cb
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-09-22 18:06:59 -0400

    Added QUIT command in main game loop

--- Program.cs
+++ Program.cs
@@ -18,3 +18,23 @@ player_name = Console.ReadLine();
 
 Console.WriteLine($"{player_name} awakens on the cold hard ground of a graveyard.");
 Console.WriteLine($"{player_name} approaches the front gate but it is guarded by a grotesque creature.");
+
+
+// Game loop
+while (is_playing)
+{
+    Console.WriteLine($"\nWhat does {player_name} do?");
+    
+    string player_input = Console.ReadLine().ToUpper();
+
+    if (player_input == "QUIT")
+    {
+        is_playing = false;
+
+        Console.WriteLine("Quitting the Game, thanks for playing!");
+    } 
+    else
+    {
+        Console.WriteLine("Unrecognized Input");
+    }
+}
