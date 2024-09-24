5c05cb8955e39875a48bd5355cf73ab9daf9d26b
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-09-24 12:57:32 -0400

    Added Go command

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 bool is_playing = true;
 
 string player_name;
-string current_location;
+string current_location = "GRAVEYARD";
 
 //Inventory
 bool has_forest_hut_key = false;
@@ -17,7 +17,7 @@ Console.WriteLine("What is the name of our protagonist?");
 player_name = Console.ReadLine();
 
 Console.WriteLine($"{player_name} awakens on the cold hard ground of a graveyard.");
-Console.WriteLine($"{player_name} approaches the front gate but it is guarded by a grotesque creature.");
+Console.WriteLine($"{player_name} approaches the front gate but it is guarded by a grotesque creature, they head back to the graveyard.");
 
 
 // Game loop
@@ -33,6 +33,56 @@ while (is_playing)
 
         Console.WriteLine("Quitting the Game, thanks for playing!");
     } 
+    else if (player_input == "GO")
+    {
+        Console.WriteLine("Go where?");
+        player_input = Console.ReadLine().ToUpper();
+
+        if (player_input == current_location)
+        {
+            Console.WriteLine("You are already at this location.");
+        }
+        else if (player_input == "GRAVEYARD")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the graveyard.");
+        }
+        else if (player_input == "GRAVEYARD HUT")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the graveyard hut.");
+        }
+        else if (player_input == "GATE")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the gate.");
+        }
+        else if (player_input == "FOREST")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the forest.");
+        }
+        else if (player_input == "FOREST HUT")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the forest hut.");
+        }
+        else if (player_input == "FISHING HUT")
+        {
+            current_location = player_input;
+
+            Console.WriteLine("Heading to the forest.");
+        } 
+        else
+        {
+            Console.WriteLine("Not a valid location.");
+        }
+    }
     else
     {
         Console.WriteLine("Unrecognized Input");
